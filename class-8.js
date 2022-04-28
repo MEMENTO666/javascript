5bb18703bf9ed735040130322877c2aaf1322caa
lee sung yoon <mementomorii666@gmail.com>
2022-04-28 17:54:27 +0900

    save

---
+++ class-8.js
@@ -0,0 +1,19 @@
+function sum(front, back) {
+
+  front + back;
+  
+  if(typeof front === ture) {
+    let result = front + back;
+    return result;
+    
+  }else {
+    console.log("front의 매개변수는 숫자가 아님");
+    
+  }
+  
+  
+  
+  
+}
+
+console.log("나는누구",3);
