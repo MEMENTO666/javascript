59f396a02ab2dceb5594acb255adfc710733e76d
lee sung yoon <mementomorii666@gmail.com>
2022-05-04 12:06:04 +0900

    save

---
+++ class-11.js
@@ -0,0 +1,30 @@
+
+
+
+// function textMachine (startValue, endValue, textValue) {
+
+//  let result = textValue ;
+
+//  for (let i = startValue; i <= endValue; i ++) {
+
+// }
+
+// return result;
+
+// }
+// console.log(textMachine(1,2,"콘솔"));
+
+// function a() {
+//   console.log("test");
+//   // return "할롱";
+// }
+
+// a();
+// console.log(a());
+
+function b (first, second) {
+  console.log(first + second);
+  // return first + second;
+}
+b(1,2);
+// console.log(b(1,2));
